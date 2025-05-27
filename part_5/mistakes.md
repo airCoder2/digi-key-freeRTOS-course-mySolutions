59eeefa1ba0f79076d9f93d82719d7eb69d84636
Abdulloh <n.solahiddin@gmail.com>
2025-05-27 07:25:49 -0500

    task seems complete

--- part_5/mistakes.md
+++ part_5/mistakes.md
@@ -26,7 +26,9 @@
     a C style string before doing any operation on it. I initialy though char c_string[] = String.c_str(); would be enough but no!
     I had to copy it for some reason.
 
+4. Subhanallah, the free memory allocaiton probelem aslo occured in this one. I could not remember what the issue was with the previous one off the top of my head, but as soon as I opened my 'mistakes.md' file, it clicked! That's when I realised it is very useful to keep them.
 
+5. One more issue that I encauntered (but did not take as much time) was how I was blocking indefinitaley upon recieving the queue. I had to change max delay to 0, so the task continues its execution instead of blocking and wating for a queue to come.
 
 
 
--- part_5/src/main.cpp
+++ part_5/src/main.cpp
@@ -19,8 +19,8 @@ static const BaseType_t app_cpu = 1;
 
 struct blinky_updated
 {
-    char message[32] = "100 more times!";
-    int times_blinked;
+  char message[32] = "100 more times!";
+  int times_blinked;
 };
 
 typedef struct blinky_updated blinky_updated;
@@ -30,116 +30,135 @@ QueueHandle_t queue_2;
 
 // should be able to read the data from serial monitor.
 // if delay xxx
-void input_accepter_interval_parser_and_queue1_updated(void* param)
+void input_accepter_interval_parser_and_queue1_updated(void *param)
 {
-    while (1)
+	blinky_updated received_struct;
+  while (1)
+  {
+    if (xQueueReceive(queue_2, &received_struct, 0) == pdTRUE)
     {
-        if (Serial.available() > 0)
+			Serial.println(received_struct.message);
+			Serial.print("Total blinks: ");
+			Serial.println(received_struct.times_blinked);
+    }
+
+    if (Serial.available() > 0)
+    {
+      String recieved = Serial.readString();
+      recieved.trim();
+      char *c_recieved = (char *) malloc(strlen(recieved.c_str()) + 1);
+      strcpy(c_recieved, recieved.c_str()); // from now on, I only work with c_recieved
+
+      char key_word[] = "delay";
+      char *key_word_position =
+          strstr(c_recieved, key_word); // search for that word inside the user input
+
+      if (key_word_position != NULL)
+      { // if the keyword 'delay' exists
+        int position =
+            (int) (key_word_position - c_recieved); // get the index of the keyword 'delay'
+        if (position == 0 && *(c_recieved + strlen(key_word)) == ' ')
         {
-            String recieved = Serial.readString();
-            recieved.trim();
-            Serial.println(recieved);
-            Serial.print("Important c++!!! ");
-            Serial.println(recieved.length());
-            char* c_recieved = (char*) malloc(strlen(recieved.c_str() + 1));
-            strcpy(c_recieved, recieved.c_str());
-            Serial.print("Important c!!! ");
-            Serial.println(strlen(c_recieved));
 
-            char key_word[] = "delay";
-            char* key_word_position =
-                strstr(c_recieved, key_word); // search for that word inside the user input
+          // we are seeing if the keyword appears at the beginning and if there
+          // is a space after the keyword itself
+          int idx = 0;
+          char interval[8]; // array to store the word after space + keyword
+          memset(interval, 0, sizeof(interval));
+          // after the space, dump the next substring until the next space
+          // inside the interval array
+          while (*(c_recieved + strlen(key_word) + idx + 1) != '\0' &&
+                 *(c_recieved + strlen(key_word) + idx + 1) != ' ')
+          {
+            interval[idx] = *(c_recieved + strlen(key_word) + idx + 1);
+            // Serial.println(idx);
+            idx++;
+          }
+          if (idx != 0)
+          { // means if while loop has been used
 
-            if (key_word_position != NULL)
-            { // if the keyword 'delay' exists
-                int position =
-                    (int) (key_word_position - c_recieved); // get the index of the keyword 'delay'
-                if (position == 0 && *(c_recieved + strlen(key_word)) == ' ')
-                {
-                    Serial.println("okay");
+            char *endptr = NULL; // I can check later if the conversion was
+                                 // sucessfull all the way
+            int interval_d = (int) strtol(interval,
+                                          &endptr,
+                                          10); // converting the parsed interval into a number
+            // Serial.print("unfiltered parse: ");
+            // Serial.println(interval_d);
 
-                    // we are seeing if the keyword appears at the beginning and if there
-                    // is a space after the keyword itself
-                    int idx = 0;
-                    char interval[8]; // array to store the word after space + keyword
-                    memset(interval, 0, sizeof(interval));
-                    // after the space, dump the next substring until the next space
-                    // inside the interval array
-                    while (*(c_recieved + strlen(key_word) + idx + 1) != '\0' &&
-                           *(c_recieved + strlen(key_word) + idx + 1) != ' ')
-                    {
-                        interval[idx] = *(c_recieved + strlen(key_word) + idx + 1);
-                        Serial.println(idx);
-                        idx++;
-                    }
-                    if (idx != 0)
-                    { // means if while loop has been used
+            if (*endptr == '\0')
+            { // if the conversion was successful (full conversion)
 
-                        char* endptr = NULL; // I can check later if the conversion was
-                                             // sucessfull all the way
-                        int interval_d =
-                            (int) strtol(interval,
-                                         &endptr,
-                                         10); // converting the parsed interval into a number
-                        Serial.print("unfiltered parse: ");
-                        Serial.println(interval_d);
-
-                        if (*endptr == '\0')
-                        { // if the conversion was successful (full conversion)
-
-                            xQueueSend(queue_1, &interval_d,
-                                       0); // put the item inside the queue
-                            Serial.println("Alhamdulillah!");
-                            vTaskDelay(pdMS_TO_TICKS(1000));
-                        }
-                        else
-                        {
-                            Serial.println("plase enter a valid interval rate. Ex: delay 100");
-                        }
-                    }
-                }
-                else
-                {
-                    Serial.println("keyword delay does not occur at the beginning of the "
-                                   "word, or does not have space after it");
-                }
+              xQueueSend(queue_1, &interval_d,
+                         0); // put the item inside the queue
+							free(c_recieved);
+              Serial.println("Alhamdulillah!");
             }
             else
             {
-                Serial.println("delay does not exist inside the text");
+              Serial.println("plase enter a valid interval rate. Ex: delay 100");
             }
+          }
         }
+        else
+        {
+          Serial.println("keyword delay does not occur at the beginning of the "
+                         "word, or does not have space after it");
+        }
+      }
+      else
+      {
+        Serial.println(c_recieved);
+				free(c_recieved);
+      }
     }
+  }
 }
 
-void interval_setter_queue2_updater(void* param)
+void interval_setter_queue2_updater(void *param)
 {
-    while (1)
+  int received_interval = 1000; // initially
+	int blinked_times = 0;
+	blinky_updated send_struct;
+  while (1)
+  {
+    if (xQueueReceive(queue_1, &received_interval, 0) == pdTRUE)
     {
-        int received_pointer;
+      Serial.print("Updated interval rate: ");
+      Serial.println(received_interval);
+    } 
 
-        if (xQueueReceive(queue_1, &received_pointer, portMAX_DELAY) == pdTRUE)
-        {
-            Serial.print("Must be the interval rate: ");
-            Serial.println(received_pointer);
-        }
-    }
+		pixels.setPixelColor(0, pixels.Color(0, 128, 128));
+    pixels.show();
+    vTaskDelay(pdMS_TO_TICKS(received_interval));
+
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+    pixels.show();
+    vTaskDelay(pdMS_TO_TICKS(received_interval));
+
+		blinked_times = blinked_times + 1;
+		
+		if ((blinked_times % 100) == 0){
+			// Serial.println("sent the struct");
+			send_struct.times_blinked = blinked_times;
+			xQueueSend(queue_2, &send_struct, portMAX_DELAY);
+		}
+
+  }
 }
 
 void setup()
 {
-    Serial.begin(9600);
-    pixels.begin();
-    pixels.setPixelColor(0, pixels.Color(255, 0, 0));
-    pixels.show();
+  Serial.begin(9600);
+  pixels.begin();
+  pixels.setPixelColor(0, pixels.Color(255, 0, 0));
+  pixels.show();
 
-    queue_1 = xQueueCreate(5, sizeof(int));
-    queue_2 = xQueueCreate(5, sizeof(blinky_updated));
+  queue_1 = xQueueCreate(5, sizeof(int));
+  queue_2 = xQueueCreate(5, sizeof(blinky_updated));
 
-    xTaskCreatePinnedToCore(
-        input_accepter_interval_parser_and_queue1_updated, "blinker", 4096, NULL, 2, NULL, app_cpu);
-    xTaskCreatePinnedToCore(
-        interval_setter_queue2_updater, "blinker", 4096, NULL, 1, NULL, app_cpu);
+  xTaskCreatePinnedToCore(
+      input_accepter_interval_parser_and_queue1_updated, "blinker", 4096, NULL, 1, NULL, app_cpu);
+  xTaskCreatePinnedToCore(interval_setter_queue2_updater, "blinker", 4096, NULL, 2, NULL, app_cpu);
 }
 
 void loop() {}
